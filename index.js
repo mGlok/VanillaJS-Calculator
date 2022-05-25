eb5d072169d450019dd74770870bb3cbe742c029
mGLOK <gloczek4lol@gmail.com>
2022-05-25 23:32:28 +0200

    Add missing class to JS scipt and name fix

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
 const numbers = document.querySelectorAll('.numbers')
-const operatos = document.querySelectorAll('.operator')
+const operators = document.querySelectorAll('.operator')
 const clearEntry = document.querySelectorAll('.clear-enter')
 const clear = document.querySelectorAll('clear')
+const remove = document.querySelectorAll('.delete')
 const equal = document.querySelectorAll('.equal')
-const previousOperand = document.querySelectorAll('.previous-operand')
-const currentOperand = document.querySelectorAll('.current-operand')
+const previousOperand = document.querySelector('.previous-operand')
+const currentOperand = document.querySelector('.current-operand')
