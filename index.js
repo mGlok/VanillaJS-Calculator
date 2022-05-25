45c98e740e1a95587d0595ae8979163e0a422c3d
mGLOK <gloczek4lol@gmail.com>
2022-05-25 23:30:14 +0200

    Download calculator items

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 'use strict';
-//todo calculator logic
+const numbers = document.querySelectorAll('.numbers')
+const operatos = document.querySelectorAll('.operator')
+const clearEntry = document.querySelectorAll('.clear-enter')
+const clear = document.querySelectorAll('clear')
+const equal = document.querySelectorAll('.equal')
+const previousOperand = document.querySelectorAll('.previous-operand')
+const currentOperand = document.querySelectorAll('.current-operand')
